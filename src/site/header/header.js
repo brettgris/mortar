42003a67adfbe471ffb89c3ef8d7046c02148e6a
Brett Grisinger <bgrisinger@costar.com>
2020-09-01 09:55:34 -0700

    Mobile

--- src/site/header/header.js
+++ src/site/header/header.js
@@ -8,24 +8,25 @@ import './header.less';
 const Header = () => {
     const [open, setOpen] = useState(false);
     return (
-        <header>
-            <div className="header-wrapper">
-                <div className="menu-btn">
-                    <button onClick={() => setOpen(!open)}>
-                        <mortar-icon kind={(open) ? 'close' : 'hamburger'}></mortar-icon>
-                    </button>
-                </div>
-                
-                <div className="logo">
-                    <Link to="/">
-                        <mortar-illustration kind="apartmentsBlack"></mortar-illustration>
-                    </Link>
-                </div>
-                
-            </div>
-            
+        <div class="header-wrapper">
+            <header>
+                <div className="header-wrapper">
+                    <div className="menu-btn">
+                        <button onClick={() => setOpen(!open)}>
+                            <mortar-icon kind={(open) ? 'close' : 'hamburger'}></mortar-icon>
+                        </button>
+                    </div>
+                    
+                    <div className="logo">
+                        <Link to="/">
+                            <mortar-illustration kind="apartmentsBlack"></mortar-illustration>
+                        </Link>
+                    </div>
+                    
+                </div>    
+            </header>
             <Menu open={open} handleClose={() => setOpen(false)} />
-        </header>
+        </div>
     )
 };
 
